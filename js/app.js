24f3bf528dde0504285c09bdb13e61d59f4a7cca
John C. Bland II <johncblandii@gmail.com>
2014-10-22 16:58:19 -0500

    Added edit/update ability

--- js/app.js
+++ js/app.js
@@ -6,7 +6,9 @@ var angularCF = angular.module("angularCF", ['ngAnimate'])
 //  SERVICES
 
     // A simple, remote http service
-    .factory("ArtService", ['$http', '$q', 'HTTP_BASE_URL', function($http, $q, HTTP_BASE_URL){
+    .factory("ArtService", ['$rootScope', '$http', '$q', 'HTTP_BASE_URL', function($rootScope, $http, $q, HTTP_BASE_URL){
+        var ART_EDIT = "edityoartyo",
+            ART_UPDATED ="artupdatedyo"; // custom event names
 
         var all = function(){
             var deferred = $q.defer();
@@ -16,9 +18,53 @@ var angularCF = angular.module("angularCF", ['ngAnimate'])
             return deferred.promise;
         };
 
+        var edit = function(art){
+            art = angular.copy(art);
+            if (art) {
+                $rootScope.$broadcast(ART_EDIT, art);
+            }
+        };
+
+        var update = function(art){
+            var deferred = $q.defer();
+
+            // Just toggle to use either approach
+            if (true){
+                $rootScope.$broadcast(ART_UPDATED, art);
+                deferred.resolve();
+            } else {
+                $http.put(HTTP_BASE_URL+'artService.json', art).success(function(){
+                    // Dispatch the event to notify listeners first
+                    $rootScope.$broadcast(ART_UPDATED, art);
+
+                    deferred.resolve.apply(this, arguments);
+                }).error(deferred.reject);
+            }
+
+            return deferred.promise;
+        };
+
+        var onArtEdit = function($scope, handler){
+            $rootScope.$on(ART_EDIT, function(event, art){
+                if (handler)
+                    handler(art);
+            });
+        };
+
+        var onArtUpdated = function($scope, handler){
+            $rootScope.$on(ART_UPDATED, function(event, art){
+                if (handler)
+                    handler(art);
+            });
+        };
+
     //  MODULE
         return {
-            all: all
+            all: all,
+            edit: edit,
+            update: update,
+            onArtEdit: onArtEdit,
+            onArtUpdated: onArtUpdated
         };
     }])
 
@@ -79,7 +125,34 @@ var angularCF = angular.module("angularCF", ['ngAnimate'])
             }
         }
 
+        $scope.editArt = ArtService.edit;
+
+        ArtService.onArtUpdated($scope, function(art){
+            for (var i = 0, len = $scope.artPieces.length; i < len; i++) {
+                if (art.ID == $scope.artPieces[i].ID) {
+                    $scope.artPieces[i] = angular.copy(art);
+                    break;
+                }
+            }
+        });
+
         init();
+    })
+
+    .controller('EditArtController', function($scope, $timeout, ArtService){
+        var editArt = this;
+
+        editArt.save = function(){
+            ArtService.update(editArt.selectedArt).then(null, function(errors){
+                alert(errors);
+            })
+        };
+
+        ArtService.onArtEdit($scope, function(art){
+            // $timeout(function(){
+                editArt.selectedArt = art;
+            // }, 0); // value just to show it update
+        });
     })
 
     // Different controller syntax
