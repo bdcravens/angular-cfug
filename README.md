5c2cb7a1ad8b2969ee73d49ef8bd4c548efbe5a9
John C. Bland II <johncblandii@gmail.com>
2014-10-23 09:08:20 -0500

    Documented the app and tweaked a few bits

--- README.md
+++ README.md
@@ -1,24 +1,30 @@
 # Angular/ColdFusion demo
-Following APIs talk to the cfartgallery database installed
-with ColdFusion 11
+Following APIs talk to the `cfartgallery` database installed with ColdFusion 11.
 
-## REST: (assuming cfugrest is the name configured in CF Admin)
+### REST: (assuming cfugrest is the name configured in CF Admin)
 
 Can pass Accept header of application/json or application/xml to
 vary returned data type
 
-* GET /rest/cfugrest/artService/ - array of structs
-* GET /rest/cfugrest/artService/1 - struct representing single art piece
-* POST /rest/cfugrest/artService/ - returns id of new art piece
+* `GET /rest/cfugrest/artService/` - array of structs
+* `GET /rest/cfugrest/artService/1` - struct representing single art piece
+* `POST /rest/cfugrest/artService/` - returns id of new art piece
 form fields: artname (string), description (string), price (numeric)
-* PUT /rest/cfugrest/artService/1
-form fields: artname (string), description (string), price (numeric)
-* DELETE /rest/cfugrest/artService/1
+* `PUT /rest/cfugrest/artService/1` form fields: artname (string), description (string), price (numeric)
+* `DELETE /rest/cfugrest/artService/1`
 
-## non-REST API - corresponds to REST API above
+### non-REST API - corresponds to REST API above
 Simple HTTP output of JSON
-* api/listart.cfm
-* api/getart.cfm?id=1
-* api/addart.cfm (same form fields as POST)
-* api/updateart.cfm?id=1 (same form fields as PUT)
-* api/deleteart.cfm?id=1
+* `api/listart.cfm`
+* `api/getart.cfm?id=1`
+* `api/addart.cfm` (same form fields as POST)
+* `api/updateart.cfm?id=1` (same form fields as PUT)
+* `api/deleteart.cfm?id=1`
+
+# Angular
+
+### Setup
+From the `angular` directory, run the following command to install all of the dependencies.
+```
+$ bower install
+```
--- angular/js/app.js
+++ angular/js/app.js
@@ -1,11 +1,18 @@
-var angularCF = angular.module("angularCF", ['ngAnimate'])
+/**
+ * Create an angular module (referenced in `ng-app` on the DOM).
+ *
+ * Below uses method chaining for simplicity and presentation purposes. It is
+ * best practice to separate your files and bring them together through some
+ * build process (grunt/gulp have a lot of tools).
+ */
+angular.module("angularCF", ['ngAnimate'])
 
-    // Set a global constant for use elsewhere
-    .constant("HTTP_BASE_URL", "http://104.131.190.213/rest/resttest/")
+    // Set a global constant for use elsewhere through dependency injection
+    .constant("HTTP_BASE_URL", "/rest/resttest/")
 
-//  SERVICES
+//  SINGLETON SERVICES
 
-    // A simple, remote http service
+    // A simple, remote http service using the module pattern
     .factory("ArtService", ['$rootScope', '$http', '$q', 'HTTP_BASE_URL', function($rootScope, $http, $q, HTTP_BASE_URL){
         var ART_EDIT = "edityoartyo",
             ART_UPDATED ="artupdatedyo"; // custom event names
@@ -13,6 +20,7 @@ var angularCF = angular.module("angularCF", ['ngAnimate'])
         var all = function(){
             var deferred = $q.defer();
 
+            // You could easily just return this line here. Using $q is purely a preference
             $http.get(HTTP_BASE_URL+'artService.json').success(deferred.resolve).error(deferred.reject);
 
             return deferred.promise;
@@ -21,6 +29,7 @@ var angularCF = angular.module("angularCF", ['ngAnimate'])
         var edit = function(art){
             art = angular.copy(art);
             if (art) {
+                // Broadcast an event from the root level scope
                 $rootScope.$broadcast(ART_EDIT, art);
             }
         };
@@ -37,6 +46,7 @@ var angularCF = angular.module("angularCF", ['ngAnimate'])
                     // Dispatch the event to notify listeners first
                     $rootScope.$broadcast(ART_UPDATED, art);
 
+                    // Resolve the promise with the arguments received in this callback
                     deferred.resolve.apply(this, arguments);
                 }).error(deferred.reject);
             }
@@ -44,15 +54,17 @@ var angularCF = angular.module("angularCF", ['ngAnimate'])
             return deferred.promise;
         };
 
+    //  EVENT HELPERS
+        // This is a convenience/helper method for subscribing to events
         var onArtEdit = function($scope, handler){
-            $rootScope.$on(ART_EDIT, function(event, art){
+            $scope.$on(ART_EDIT, function(event, art){
                 if (handler)
                     handler(art);
             });
         };
 
         var onArtUpdated = function($scope, handler){
-            $rootScope.$on(ART_UPDATED, function(event, art){
+            $scope.$on(ART_UPDATED, function(event, art){
                 if (handler)
                     handler(art);
             });
@@ -68,7 +80,7 @@ var angularCF = angular.module("angularCF", ['ngAnimate'])
         };
     }])
 
-    // A simple, global data management service
+    // A "local" data management service singleton
     .factory("CartService", ['$rootScope', '$http', function($rootScope, $http){
         var items = [],
             CART_UPDATED = "CART::UPDATED"; // custom event name
@@ -79,6 +91,7 @@ var angularCF = angular.module("angularCF", ['ngAnimate'])
             $rootScope.$broadcast(CART_UPDATED, items.length);
         };
 
+    //  EVENT HELPERS
         var onCartUpdate = function($scope, handler){
             $scope.$on(CART_UPDATED, function(event, length){
                 if (handler)
@@ -89,7 +102,7 @@ var angularCF = angular.module("angularCF", ['ngAnimate'])
     //  MODULE
         return {
             add: add,
-            items: function(){
+            items: function(){ // just showing you can have a function right here
                 return items;
             },
             onCartUpdate: onCartUpdate
@@ -98,7 +111,7 @@ var angularCF = angular.module("angularCF", ['ngAnimate'])
 
 //  CONTROLLERS
 
-    // Using $scope
+    // A controller using $scope to get data to the DOM
     .controller("ArtController", function($scope, ArtService, CartService){
         $scope.artPieces = null;
 
@@ -117,6 +130,7 @@ var angularCF = angular.module("angularCF", ['ngAnimate'])
         };
 
         $scope.deleteArt = function(art){
+            // Absolutely not the way I'd normally do it. Just for preso purposes.
             for (var i = 0, len = $scope.artPieces.length; i < len; i++) {
                 if (art.ID == $scope.artPieces[i].ID) {
                     $scope.artPieces.splice(i, 1);
@@ -125,9 +139,17 @@ var angularCF = angular.module("angularCF", ['ngAnimate'])
             }
         }
 
+        // Some methods on the scope are just pass through methods
+        // like this case where we just want to call ArtService without
+        // having to write extra code like `addToCart` above.
         $scope.editArt = ArtService.edit;
 
+    //  EVENT LISTENERS
+        // Use the event helper on `ArtService` to subscribe to art update events.
+        // This approach has the advantage of not having to mess with `$rootScope`
+        // and not having to know the event names being dispatched.
         ArtService.onArtUpdated($scope, function(art){
+            // Again, another time I wouldn't do this normally.
             for (var i = 0, len = $scope.artPieces.length; i < len; i++) {
                 if (art.ID == $scope.artPieces[i].ID) {
                     $scope.artPieces[i] = angular.copy(art);
@@ -136,33 +158,49 @@ var angularCF = angular.module("angularCF", ['ngAnimate'])
             }
         });
 
+        // Initialize the controller (see above)
         init();
     })
 
+    // A controller not using `$scope` to get data to the DOM
     .controller('EditArtController', function($scope, $timeout, ArtService){
-        var editArt = this;
+        // By creating this we always have a way of getting the context of the
+        // controller. See more here: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/this
+        var self = this;
 
-        editArt.save = function(){
-            ArtService.update(editArt.selectedArt).then(null, function(errors){
+        // A public method available on the DOM
+        self.save = function(){
+            ArtService.update(self.selectedArt).then(null, function(errors){
                 alert(errors);
             })
         };
 
+    //  EVENT LISTENERS
+        // Subscribe to edit events
         ArtService.onArtEdit($scope, function(art){
+            // If you uncomment this `$timeout`, you can see a delayed DOM
+            // update in the modal.
+
             // $timeout(function(){
-                editArt.selectedArt = art;
-            // }, 0); // value just to show it update
+                self.selectedArt = art;
+            // }, 1000);
         });
     })
 
-    // Different controller syntax
+    // Another controller not using `$scope` to provide DOM access
     .controller("CartController", function($scope, CartService){
-        var cart = this; // simple helper for avoiding `this` issues
-        cart.cartLength = 0;
-        cart.items = [];
+        var self = this; // simple helper for avoiding `this` issues
+        self.cartLength = 0; // Public variable available in the DOM
+        self.items = []; // Public variable available in the DOM
 
+    //  EVENT LISTENERS
         CartService.onCartUpdate($scope, function(length){
-            cart.cartLength = length;
-            cart.items = CartService.items();
+            self.cartLength = length; // Set the length to update the DOM
+
+            // Get the latest items from the cart. This isn't used in the DOM
+            // but you could switch out the `{{cartLength}}` binding to use
+            // {{items.length}} to see the array. You can also spit out the
+            // array using the `json` filter `{{items | json}}`.
+            self.items = CartService.items();
         });
     });
